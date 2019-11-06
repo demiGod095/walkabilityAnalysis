a76918627ab5636bc98b9f75f095b0af9edc7010
Shreyas Walvekar <shreyas.whis@gmail.com>
2019-11-06 17:04:35 +1100

    added zScore scaling codes

---
+++ data-formatting-python/scaleToSA2.py
@@ -0,0 +1,25 @@
+#To find mean zscore for sa2 level
+
+import pandas as pd
+import json  
+from pandas.io.json import json_normalize  
+
+with open('zScore-greaterMelb_noGeo.json') as f:
+    nested = json.load(f)
+
+data = pd.DataFrame.from_dict(json_normalize(nested))
+
+data = data['features']
+
+df = pd.DataFrame.from_records(data[0])
+df = df['properties']
+df1 = pd.DataFrame.from_records(df)
+
+df1 = df1[['SA2_MAIN16','SA2_NAME16','SumZScore']]
+
+df2 = df1.groupby(['SA2_MAIN16','SA2_NAME16'],as_index=False)['SumZScore'].mean()
+
+print(df2)
+
+df2.to_json(r'SA2ZScore.json', orient = "records", date_format = "epoch", double_precision = 10, force_ascii = True, date_unit = "ms", default_handler = None)
+
